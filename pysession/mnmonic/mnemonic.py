99c871210501123840d2cc2190f2c40dcd3d7143
Arjan de Haan <agdehaan@gmail.com>
2021-05-30 17:53:29 +0200

    retrieve prefix length from language file

--- pysession/mnmonic/mnemonic.py
+++ pysession/mnmonic/mnemonic.py
@@ -10,20 +10,29 @@ class MnemonicError(FileNotFoundError):
 
 
 class KeyPair:
-    def __init__(self, words, version, language="english"):
+    def __init__(self, words: list, version: int, language: str = "english"):
+        self.words = words
         current = os.path.dirname(os.path.abspath(__file__))
         mnemonic_path = os.path.join(current, "languages", f"{language}.json")
-
         try:
             # Attempt reading the file
             with open(mnemonic_path, "r") as mnemonic_file:
                 mnemonic_data = mnemonic_file.read()
-            mnemonic_words = json.loads(mnemonic_data)
+
+            # Read the json file
+            self.wordset = json.loads(mnemonic_data)
+
+            # Verify if all required keys are in the language file
+            if not all(key in self.wordset for key in ("prefix-length", "words")):
+                raise MnemonicError("Invalid language file")
+
+        # Handle general parsing exceptions
         except FileNotFoundError as e:
             raise MnemonicError("Language not found") from e
         except json.decoder.JSONDecodeError as e:
-            raise MnemonicError("Language file could not be decoted") from e
+            raise MnemonicError("Language file could not be decoded") from e
 
+    @classmethod
     def from_words(words, **kwargs):
         pass
 
@@ -38,6 +47,3 @@ class KeyPair:
     @classmethod
     def new_keys(language: str = "english"):
         pass
-
-
-KeyPair("", "")
