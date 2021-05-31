731ba99f9c675ea7d8fea1022fcbc82a59c009af
Arjan de Haan <agdehaan@gmail.com>
2021-05-31 08:48:18 +0200

    Verify seed

--- pysession/mnmonic/mnemonic.py
+++ pysession/mnmonic/mnemonic.py
@@ -10,7 +10,9 @@ class MnemonicError(FileNotFoundError):
 
 
 class KeyPair:
-    def __init__(self, words: list, version: int, language: str = "english"):
+    checksum = ''
+
+    def __init__(self, words: list, version: int = 3, language: str = "english"):
         self.words = words
         current = os.path.dirname(os.path.abspath(__file__))
         mnemonic_path = os.path.join(current, "languages", f"{language}.json")
@@ -26,15 +28,34 @@ class KeyPair:
             if not all(key in self.wordset for key in ("prefix-length", "words")):
                 raise MnemonicError("Invalid language file")
 
+            self._verify_memonic()
+
         # Handle general parsing exceptions
         except FileNotFoundError as e:
             raise MnemonicError("Language not found") from e
         except json.decoder.JSONDecodeError as e:
             raise MnemonicError("Language file could not be decoded") from e
 
+    def _verify_memonic(self):
+        # TODO finish
+        # TODO: check if this works for other languages
+        word_count = len(self.words)
+        prefix_length = self.wordset["prefix-length"]
+        if word_count < 12:
+            raise MnemonicError("Mnemonic seed is too short")
+        elif (prefix_length == 0 and word_count % 3 != 0 or prefix_length > 0) and (
+            prefix_length > 0 and word_count % 3 == 2
+        ):
+            raise MnemonicError("Mnemonic seed is too short")
+        elif prefix_length > 0 and word_count % 3 == 0:
+            raise MnemonicError("Last word in Menmonic seed is missing")
+        elif(prefix_length > 0):
+            self.checksum = self.words.pop()
+        
+
     @classmethod
-    def from_words(words, **kwargs):
-        pass
+    def from_words(cls, words, **kwargs):
+        return KeyPair(words)
 
     @classmethod
     def from_file(path: str = "mnemonic.json", **kwargs):
