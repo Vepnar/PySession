d4d0bb32c74fb67ebbfa32ad77ec2c7d03d719e7
Arjan de Haan <agdehaan@gmail.com>
2021-06-26 18:14:20 +0200

    Added temp util network connector

---
+++ pysession/networking/util.py
@@ -0,0 +1,41 @@
+import aiohttp
+
+# TODO: untested codebase
+
+async def _make_request(url: str, **kwargs: dict) -> aiohttp.ClientResponse:
+    """Creates an ASYNC session and posts the given url
+
+    Note: Close the session after using.
+    """
+    # Inject timeout of 30 seconds
+    if 'timeout' not in kwargs:
+        kwargs['timeout'] = 30
+
+    async with aiohttp.ClientSession() as session:
+        return session.post(url, **kwargs)
+
+async def request_text(url: str, **kwargs: dict) -> str:
+    async with _make_request(url, **kwargs) as response:
+        return response.text()
+
+async def request_json(url: str, **kwargs: dict) -> dict:
+    async with _make_request(url, **kwargs) as response:
+        return response.json()
+
+async def request_buffer(url: str, **kwargs: dict) -> aiohttp.StreamReader:
+    async with make_request(url, **kwargs) as response:
+            return await response.content
+
+async def request_jsonrpc(url: str, method : dict, params: dict, options: dict = {}):
+    if not url:
+        raise Exception("No url given")
+
+    body = {
+        'jsonrpc' : '2.0',
+        'id' : '0',
+        'method': method,
+        'params': params
+    }
+
+    headers = {'Content-Type': 'application/json'}
+    return await request_json(url, json=body, headers=headers)
