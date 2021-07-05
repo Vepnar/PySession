4c5bde1136c7f7659c52629c56066d3d048b3726
Arjan de Haan <agdehaan@gmail.com>
2021-07-05 17:53:45 +0200

    Implemented ask public key

--- pysession/networking/swarm.py
+++ pysession/networking/swarm.py
@@ -2,7 +2,7 @@ import os
 import json
 import time
 import random
-import typing
+import asyncio
 
 from .util import request_jsonrpc
 
@@ -16,7 +16,6 @@ class SwarmException(Exception):
 
 class Swarm:
     def __init__(self):
-
         # Create working variables
         self.swarm_url_lock = asyncio.Lock()
         self.swarm_map = {}
@@ -24,16 +23,15 @@ class Swarm:
 
         # Load seed for the swarm
         current = os.path.dirname(os.path.abspath(__file__))
-        node_path = os.path.join(current, "cryptography", "node_list.json")
+        node_path = os.path.join(current, "node_list.json")
         with open(node_path, "r") as node_file:
-            self.seed_node_list = json.loads(node_file)
+            self.seed_node_list = json.load(node_file)
 
     async def storage_servers_from_seed(self) -> list:
         if self.storage_server_seed_cache:
             return self.storage_server_seed_cache
 
         seed = random.choice(self.seed_node_list)
-
         params = {
             "active_only": True,
             "limit": 5,  # Request only 5 for now.
@@ -42,16 +40,21 @@ class Swarm:
                 "storage_port": True,
             },
         }
-        result = await request_jsonrpc(seed.url, "get_n_service_nodes", params)
-        result = filter(lambda node: node.public_ip != "0.0.0.0", result)
-        result = map(
-            lambda node: f"https://{node.public_ip}:{node.storage_port}/storage_rpc/v1"
+        response = await request_jsonrpc(seed["url"], "get_n_service_nodes", params)
+
+        storage_nodes = filter(
+            lambda x: x['public_ip'] != "0.0.0.0",
+            response["result"]["service_node_states"],
+        )
+        storage_nodes = map(
+            lambda x: f"https://{x['public_ip']}:{x['storage_port']}/storage_rpc/v1",
+            storage_nodes,
         )
 
         # Cache to speed up the progress
-        self.storage_server_seed_cache = result
+        self.storage_server_seed_cache = list(storage_nodes)
 
-        return result
+        return self.storage_server_seed_cache
 
     async def storage_servers_from_service_nodes(self) -> list:
         node_url = await self.random_service_node(True)
@@ -63,28 +66,34 @@ class Swarm:
             "fields": {"public_ip": True, "storage_port": True},
         }
 
-        result = await request_jsonrpc(
+        response = await request_jsonrpc(
             node_url, "oxend_request", params, options={"snode": True}
         )
-        result = filter(lambda node: node.public_ip != "0.0.0.0", result)
-        result = map(
-            lambda node: f"https://{node.public_ip}:{node.storage_port}:/storage_rpc/v1"
+        storage_nodes = filter(
+            lambda node: node.public_ip != "0.0.0.0",
+            response["result"]["service_node_states"],
         )
-        return result
+        storage_nodes = map(
+            lambda node: f"https://{node.public_ip}:{node.storage_port}/storage_rpc/v1",
+            storage_nodes,
+        )
+        return storage_nodes
 
     async def random_service_node(self, use_seed: bool = False) -> str:
         storage_nodes = [x for nodes in self.swarm_map.snodes for x in nodes]
         storage_nodes = map(
-            lambda node: f"https://{node.ip}:{node.port}/storage_rpc/v1"
+            lambda node: f"https://{node.ip}:{node.port}/storage_rpc/v1", storage_nodes
         )
 
         if use_seed:
             storage_nodes.append(await self.storage_servers_from_seed())
         else:
             storage_nodes.append(await self.storage_servers_from_service_nodes())
-        return random.choice(storage_nodes)
 
-    async def get_swarm_node_url(self, public_key) -> str:
+        # Random node without duplicates.
+        return random.choice(set(storage_nodes))
+
+    async def get_swarm_node_url(self, public_key: str) -> str:
         """Retrieve a single swarm node url"""
         if not public_key or len(public_key) < 66:
             raise Exception(f"Invalid public key: {public_key}, {len(public_key)}")
@@ -95,7 +104,7 @@ class Swarm:
         ):
             async with self.swarm_url_lock:
                 node_url = await self.random_storage_node(False)
-                node_data = await self.ublic_key_ask(
+                node_data = await self.public_key_ask(
                     node_url, "get_snode_for_pubkey", public_key
                 )
                 if not node_data:
@@ -107,5 +116,30 @@ class Swarm:
                         f"Could not get storage nodes object from {node_url}, {public_key}"
                     )
 
-                random_node = random.choice(self.swarm_map[public_key].snodes)
-                return f"https://{random_node.ip}:{random_node.port}/storage_rpc/v1"
+        random_node = random.choice(self.swarm_map[public_key].snodes)
+        return f"https://{random_node.ip}:{random_node.port}/storage_rpc/v1"
+
+    # TODO: from node-session-client. We have the pubkey we can just internally look up the url?
+    # TODO maybe do 3 queries here and show confirmiations.s
+    async def ask_public_key(self, url, method, public_key, params={}):
+        # Handle exception
+        async with request_jsonrpc(
+            url, method, {**params, "pubKey": public_key}, ignore_self_signed=True
+        ) as result:
+            if "snodes" in result:
+                self.swarm_map[public_key] = {
+                    "updated_at": time.time(),
+                    "snodes": result["snodes"],
+                }
+
+                if method != "get_snodes_for_pubkey":
+                    # TODO: logging. swarm reorg is not valid
+                    return await self.ask_public_key(
+                        await self.get_swarm_node_url(public_key),
+                        method,
+                        public_key,
+                        params,
+                    )
+
+            # TODO: Implement message retrieving
+            return result
