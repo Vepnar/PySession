5265762dc5ecae891151af32e6d59fc10fa55aab
Arjan de Haan <agdehaan@gmail.com>
2021-07-09 13:53:31 +0200

    Renamed variables

--- tests/networking/util.py
+++ tests/networking/util.py
@@ -6,51 +6,51 @@ from pysession.networking.util import request_json, request_stream, request_text
 
 
 @pytest.fixture
-def http_bin_url():
-    return "https://httpbin.org/"
+def httpbin_url():
+    return "https://httpbin.org"
 
 
 @pytest.mark.asyncio
-async def test_valid_json(http_bin_url):
-    result = await request_json(http_bin_url + "/anything")
+async def test_valid_json(httpbin_url):
+    result = await request_json(httpbin_url + "/anything")
 
     assert "method" in result
     assert result["method"] == "POST"
 
 
 @pytest.mark.asyncio
-async def test_invalid_json(http_bin_url):
+async def test_invalid_json(httpbin_url):
     try:
-        await request_json(http_bin_url + "/status/200")
+        await request_json(httpbin_url + "/status/200")
         assert False
     except ContentTypeError:
         pass
 
 
 @pytest.mark.asyncio
-async def test_error_request(http_bin_url):
+async def test_error_request(httpbin_url):
     """Test if the buildin error status code checker works"""
     try:
-        await request_text(http_bin_url + "/status/500")
+        await request_text(httpbin_url + "/status/500")
         assert False
     except AssertionError:
         pass
 
 
 @pytest.mark.asyncio
-async def test_http_timeout(http_bin_url):
+async def test_http_timeout(httpbin_url):
     """Test a timeout of 10 seconds. This should be 31 seconds to work"""
-    await request_text(http_bin_url + "/delay/10")
+    await request_text(httpbin_url + "/delay/10")
 
 
 @pytest.mark.asyncio
-async def test_valid_text(http_bin_url):
-    result = await request_text(http_bin_url + "/status/200")
+async def test_valid_text(httpbin_url):
+    result = await request_text(httpbin_url + "/status/200")
     assert not result
 
 
 @pytest.mark.asyncio
-async def test_valid_buffer(http_bin_url):
+async def test_valid_buffer(httpbin_url):
     """Test http streams"""
 
     async def handle_stream(stream):
@@ -60,5 +60,5 @@ async def test_valid_buffer(http_bin_url):
             buffer += data.decode("UTF-8")
         return buffer
 
-    buffer = await request_stream(http_bin_url + "/anything", handle_stream)
-    assert "https://httpbin.org/anything" in buffer
+    buffer = await request_stream(httpbin_url + "/anything", handle_stream)
+    assert httpbin_url + "/anything" in buffer
